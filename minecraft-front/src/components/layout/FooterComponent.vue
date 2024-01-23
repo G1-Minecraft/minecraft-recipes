4cb7f340c0d8a6499ec6ac3b75ec6c326b1b6952
Machu Alexandre <alexandre.machu@etu.umontpellier.fr>
2024-01-23 14:37:01 +0100

    Composants crées (header / footer), ne pas oublier de mettre la font dans le main après

--- minecraft-front/src/components/layout/FooterComponent.vue
+++ minecraft-front/src/components/layout/FooterComponent.vue
@@ -1,10 +1,23 @@
-<script setup lang="ts">
-  import '@/assets/styles/footer.css'
-</script>
-
 <template>
   <footer>
     <h1>Site de craft pour Minecraft non officiel, non approuvé et non associé à Mojang ou Microsoft</h1>
-    <img id="footer-background-svg" src="../../assets/images/background/footer-background.svg" alt="FooterBackground">
   </footer>
 </template>
+
+<style scoped>
+footer{
+  background-image: url("@/assets/images/background/footer-background.svg");
+  height: 93px;
+  flex-shrink: 0;
+}
+
+h1{
+  color: #FFF;
+  font-family: NotoSans,serif;
+  padding: 30px 0 0 0;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+</style>
--- minecraft-front/src/components/layout/HeaderComponent.vue
+++ minecraft-front/src/components/layout/HeaderComponent.vue
@@ -1,10 +1,36 @@
-<script setup lang="ts">
-import '@/assets/styles/header.css'
-</script>
-
 <template>
 <header>
-  <h1>Header</h1>
-  <img id="header-background-svg" src="../../assets/images/background/header-background.svg" alt="HeaderBackground">
+  <nav class="menu">
+    <a> Connexion </a>
+    <a> Inscription </a>
+  </nav>
 </header>
 </template>
+
+<style scoped>
+header{
+  background-image: url("@/assets/images/background/header-background.svg");
+  height: 100px;
+  flex-shrink: 0;
+}
+
+nav{
+  padding: 30px 30px 0 0;
+  display: flex;
+  height: 100px;
+  justify-content: flex-end;
+  align-items: flex-start;
+  gap: 41px;
+  flex-shrink: 0;
+}
+
+a{
+  color: #FFF;
+  font-family: NotoSans,serif;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+</style>
