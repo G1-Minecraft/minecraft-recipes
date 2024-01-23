2a3b3862f9dc11de0f1596bee6c4f424f41f2513
Machu Alexandre <alexandre.machu@etu.umontpellier.fr>
2024-01-23 11:36:21 +0100

    Début de la fonctionnalité

---
+++ minecraft-front/src/components/HeaderComponent.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import '@/assets/header.css'
+</script>
+
+<template>
+<header>
+  <h1>Header</h1>
+  <img id="header-background-svg" src="../assets/header-background.svg" alt="HeaderBackground">
+</header>
+</template>
