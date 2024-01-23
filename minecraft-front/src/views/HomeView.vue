e0205ff55a0c3ebf78f123ac70afa0bbea4d5e9e
Colfeo <alex.loumach@gmail.com>
2024-01-23 11:52:39 +0100

    Debut de la page d'accueil (#18)
    Co-authored-by: Hugo Siliveri <hugo.siliveri@etu.umontpellier.fr>

--- minecraft-front/src/views/HomeView.vue
+++ minecraft-front/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+        <main>
+
+        </main>
+        <aside>
+
+        </aside>
+    </div>
 </template>
