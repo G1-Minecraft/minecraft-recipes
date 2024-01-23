b7226d7ebe16088a8f27918a816ae814f434f2e8
Machu Alexandre <alexandre.machu@etu.umontpellier.fr>
2024-01-23 12:01:41 +0100

    Reforge de la structure du projet

--- minecraft-front/src/components/layout/FooterComponent.vue
+++ minecraft-front/src/components/layout/FooterComponent.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-  import '@/assets/footer.css'
+  import '@/assets/styles/footer.css'
 </script>
 
 <template>
   <footer>
     <h1>Site de craft pour Minecraft non officiel, non approuvé et non associé à Mojang ou Microsoft</h1>
-    <img id="footer-background-svg" src="../assets/footer-background.svg" alt="FooterBackground">
+    <img id="footer-background-svg" src="../../assets/images/background/footer-background.svg" alt="FooterBackground">
   </footer>
 </template>
