c328b3c25dad612353f75b545c5b46de58a8c9bb
Colfeo <alex.loumach@gmail.com>
2024-01-23 12:02:28 +0100

    Reforge de la structure du projet (#19)

---
+++ minecraft-front/src/components/layout/HeaderComponent.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import '@/assets/styles/header.css'
+</script>
+
+<template>
+<header>
+  <h1>Header</h1>
+  <img id="header-background-svg" src="../../assets/images/background/header-background.svg" alt="HeaderBackground">
+</header>
+</template>
