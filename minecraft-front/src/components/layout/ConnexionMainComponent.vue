56623f984c69acc87d19b2884b755724577e5fcf
Colfeo <alex.loumach@gmail.com>
2024-01-23 16:28:15 +0100

    Début des composants de connexion / inscription (#23)

---
+++ minecraft-front/src/components/layout/ConnexionMainComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <form action="" method="get" class="form-example">
+      <div class="form-example">
+        <label for="name">Nom d'utilisateur</label>
+        <input type="text" name="name" id="name" required />
+      </div>
+      <div class="form-example">
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" id="password" required />
+      </div>
+      <div class="form-example">
+        <input type="submit" value="Subscribe!" />
+      </div>
+    </form>
+  </main>
+</template>
+
+<style scoped>
+main{
+  background-image: url("@/assets/images/background/form-connexion-background.svg");
+  width: 75%;
+  flex-shrink: 0;
+}
+</style>
+<script setup lang="ts">
+</script>
---
+++ minecraft-front/src/views/ConnexionView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import ConnexionMainComponent from "@/components/layout/ConnexionMainComponent.vue";
+</script>
+
+<template>
+  <div class="connexionView">
+    <ConnexionMainComponent />
+  </div>
+</template>
+
+<style scoped>
+.connexionView{
+  display: flex;
+  padding: 2% 0;
+  width: 100%;
+  justify-content: center;
+}
+</style>
