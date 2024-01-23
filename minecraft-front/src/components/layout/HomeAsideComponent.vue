db04eaa7107b48c44fd02f8afd32177d700611a2
Colfeo <alex.loumach@gmail.com>
2024-01-23 16:26:25 +0100

    Debut de la page d'accueil (#22)
    Co-authored-by: Hugo Siliveri <hugo.siliveri@etu.umontpellier.fr>

---
+++ minecraft-front/src/components/layout/HomeAsideComponent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import ItemsListComponent from "@/components/elements/ItemsListComponent.vue";
+  import SearchBarComponent from "@/components/elements/SearchBarComponent.vue";
+</script>
+
+<template>
+  <aside>
+    <ItemsListComponent />
+    <SearchBarComponent />
+  </aside>
+</template>
+
+<style scoped>
+  aside {
+    display: flex;
+    flex-direction: column-reverse;
+    width: 34%;
+    border: 1px solid #000;
+    background: rgba(0, 0, 0, 0.75);
+  }
+</style>
