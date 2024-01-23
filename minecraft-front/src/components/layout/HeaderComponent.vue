fc700caceae3302a0a4512914117143c5b7f43a9
Machu Alexandre <alexandre.machu@etu.umontpellier.fr>
2024-01-23 16:27:37 +0100

    Début des composants de connexion / inscription

--- minecraft-front/src/components/layout/HeaderComponent.vue
+++ minecraft-front/src/components/layout/HeaderComponent.vue
@@ -1,8 +1,13 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
 <template>
 <header>
   <nav class="menu">
-    <a> Connexion </a>
-    <a> Inscription </a>
+    <div class="imgAccueil"><img src="@/assets/images/minecraft-crafting-table.png" alt="logo" @click="router.push({name: 'home'})" /></div>
+    <div class="textConnexion" @click="router.push({name: 'connexion'})">Connexion</div>
+    <div class="textRegister" @click="router.push({name: 'register'})">Inscription</div>
   </nav>
 </header>
 </template>
@@ -15,7 +20,6 @@ header{
 }
 
 nav{
-  padding: 30px 30px 0 0;
   display: flex;
   height: 100px;
   justify-content: flex-end;
@@ -24,7 +28,27 @@ nav{
   flex-shrink: 0;
 }
 
-a{
+img{
+  width: 50px;
+  flex-shrink: 0;
+}
+
+.imgAccueil{
+  padding: 20px 75% 0 0;
+}
+
+.textConnexion{
+  padding: 30px 0 0 0;
+  color: #FFF;
+  font-family: NotoSans,serif;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+.textRegister{
+  padding: 30px 30px 0 0;
   color: #FFF;
   font-family: NotoSans,serif;
   font-size: 18px;
---
+++ minecraft-front/src/views/RegisterView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+main{
+  background-image: url("@/assets/images/background/form-inscription-background.svg");
+
+}
+</style>
