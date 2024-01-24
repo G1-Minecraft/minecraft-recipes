6b5b160c5586c6fe80a7867092ffb6bf19c11863
Hugo Siliveri <hugo.siliveri@etu.umontpellier.fr>
2024-01-24 12:08:29 +0100

    Fix responsive du header et footer

--- minecraft-front/src/components/layout/HeaderComponent.vue
+++ minecraft-front/src/components/layout/HeaderComponent.vue
@@ -1,31 +1,35 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-const router = useRouter()
+  import { useRouter } from 'vue-router'
+  const router = useRouter()
 </script>
+
 <template>
-<header>
-  <nav class="menu">
-    <div class="imgAccueil"><img src="@/assets/images/minecraft-crafting-table.png" alt="logo" @click="router.push({name: 'home'})" /></div>
-    <div class="textConnexion" @click="router.push({name: 'connexion'})">Connexion</div>
-    <div class="textRegister" @click="router.push({name: 'register'})">Inscription</div>
-  </nav>
-</header>
+  <header>
+    <nav>
+      <div class="imgAccueil">
+        <img src="@/assets/images/minecraft-crafting-table.png" alt="logo" @click="router.push({name: 'home'})" />
+      </div>
+      <div class="infos">
+        <div class="text" @click="router.push({name: 'connexion'})">Connexion</div>
+        <div class="text" @click="router.push({name: 'register'})">Inscription</div>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <style scoped>
 header{
+  display: flex;
   background-image: url("@/assets/images/background/header-background.svg");
-  height: 100px;
+  height: 80px;
   flex-shrink: 0;
 }
 
-nav{
+nav {
   display: flex;
-  height: 100px;
-  justify-content: flex-end;
-  align-items: flex-start;
-  gap: 41px;
-  flex-shrink: 0;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
 }
 
 img{
@@ -33,22 +37,21 @@ img{
   flex-shrink: 0;
 }
 
-.imgAccueil{
-  padding: 20px 75% 0 0;
+.imgAccueil {
+  display: flex;
+  align-items: center;
+  padding-left: 2%;
 }
 
-.textConnexion{
-  padding: 30px 0 0 0;
-  color: #FFF;
-  font-family: NotoSans,serif;
-  font-size: 18px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
+.infos {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 20%;
 }
 
-.textRegister{
-  padding: 30px 30px 0 0;
+.text{
   color: #FFF;
   font-family: NotoSans,serif;
   font-size: 18px;
