f4901ac9e8cfa1fe0e0451176ae8ea48504f3bf6
MiniGarreto <minigarreto@gmail.com>
2024-01-28 23:17:49 +0100

    Creation de crafts (incomplet)
    (cherry picked from commit d71baa16ed6ac85679b4f6afec7654baa045f507)

---
+++ minecraft-front/src/components/layout/CraftCreationComponent.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import HomeMainComponent from "@/components/layout/HomeMainComponent.vue";
+import ItemsListComponent from "@/components/layout/ItemsListComponent.vue";
+</script>
+
+<template>
+  <h1>Création de craft</h1>
+  <HomeMainComponent></HomeMainComponent>
+  <ItemsListComponent></ItemsListComponent>
+  <button type="submit" class="minecraftBtn">Envoyer</button>
+</template>
+
+<style scoped>
+
+h1 {
+  color: black;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+button {
+  width: 20%;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+</style>
